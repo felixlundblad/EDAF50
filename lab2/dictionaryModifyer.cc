faa7edad405c7466605f1d272836c52b3905d07d
I_am_Nyx <felixlundbladh@gmail.com>
2018-02-02 12:38:32 +0100

    words.txt är nu endast små bokstäver

--- lab2/dictionaryModifyer.cc
+++ lab2/dictionaryModifyer.cc
@@ -1,44 +1,43 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-
     string line;
     char c;
     ifstream inputFile("/home/felix/Skrivbord/words");
     ofstream outputFile("words.txt");
-    //if(inputFile.is_open() && outputFile.is_open()){
     while (getline(inputFile, line))
     {
-        //cout << line << line.length() << endl;
         if (line.length() < 3)
         {
-            outputFile << line << " " << 0;
+
+            for(int i = 0; i < line.length(); i++)
+                outputFile << static_cast<char>(tolower(line.at(i)));
+            outputFile << " " << 0;
         }
         else
         {
-            outputFile << line;
-            outputFile << " "; 
+            for(int i = 0; i < line.length(); i++)
+                outputFile << static_cast<char>(tolower(line.at(i)));
+            outputFile << " ";
             outputFile << line.length() - 2;
             outputFile << " ";
             for (int j = 0; j < line.length() - 2; j++)
             {
                 for (int i = 0; i < 3; i++)
                 {
-                    outputFile << line.at(j + i);
+
+                    outputFile << static_cast<char>(tolower(line.at(j + i)));
                 }
                 outputFile << " ";
             }
         }
         outputFile << '\n';
     }
-    /*}else{
-        std::cout << "Unable to open at least one of the files" << std::endl;
-        return -1;
-    }*/
     return 0;
 }
